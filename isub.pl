d8219962a28c8693fae647162fdc99af7a0a5603
David <david@ubuntu.ubuntu-domain>
2013-09-15 15:07:47 -0400

    Several small changes, as follows:
    - Whitespace normalization
    - Removed 'doall' and 'filter' options
      since they weren't cleanly conceived
    - Added ability to skip entire files.

--- isub.pl
+++ isub.pl
@@ -16,18 +16,15 @@ perl isub.pl [options] <pattern> <repl> <dir>
 
 options:
 
-  --help              Print this message and exit
-  --doall             Perform all replacements without asking
-  --filter <regex>    Only process filenames that match <regex>
+  --help         Print this message and exit
+
 =cut
 
 
 # Parse command line
 my $help;
-my $filename_filter = ".";
 
-GetOptions("help"     => \$help,
-           "filter=s" => \$filename_filter;
+GetOptions("help" => \$help);
 
 my $extension;
 
@@ -53,11 +50,10 @@ my %AutoReplace = ();
 #
 ##
 sub apply_transform_to_dir {
-	my ($pattern, $repl, $dir) = @_;
+    my ($pattern, $repl, $dir) = @_;
     for my $file (`find -L $dir`) {
         chomp $file;
-        next unless $file =~ m/$filename_filter/;
-    	apply_transform_to_file($pattern, $repl, $file);
+        apply_transform_to_file($pattern, $repl, $file);
     }
 }
 
@@ -70,19 +66,19 @@ sub apply_transform_to_dir {
 #
 ##
 sub apply_transform_to_file {
-	my ($pattern, $repl, $file) = @_;
+    my ($pattern, $repl, $file) = @_;
     print "Process file \"$file\"? [Y|n]: ";
     chomp(my $answer = lc <STDIN>);
     return if $answer =~ m/^n$/;
-	open(my $input_fh, '<', $file) or die "Could not open $file: $!\n";
-	my $transformed_file = '';
-	for my $line (<$input_fh>) {
+    open(my $input_fh, '<', $file) or die "Could not open $file: $!\n";
+    my $transformed_file = '';
+    for my $line (<$input_fh>) {
         $transformed_file .= apply_transform_to_line($pattern, $repl, $line);
-	}
+    }
     close($input_fh);
-	open(my $output_fh, '>', $file) or die "Could not open $file: $!\n";
-	print $output_fh $transformed_file;
-	close($output_fh);
+    open(my $output_fh, '>', $file) or die "Could not open $file: $!\n";
+    print $output_fh $transformed_file;
+    close($output_fh);
 }
 
 
@@ -94,29 +90,29 @@ sub apply_transform_to_file {
 #
 ##
 sub apply_transform_to_line {
-	my ($pattern, $repl, $line) = @_;
-	if ($line =~ m/($pattern)/) {
+    my ($pattern, $repl, $line) = @_;
+    if ($line =~ m/($pattern)/) {
         my $match = $1;
         if (defined $AutoReplace{$1}) {
-		    $line =~ s/$pattern/$repl/;
+            $line =~ s/$pattern/$repl/;
         } else {
-        	my ($beg, $end) = ($-[0], $+[0]);
-        	my $prebold  = substr($line, 0, $beg);
-        	my $boldtext = substr($line, $beg, length $1);
-        	my $postbold = substr($line, $end);
-        	print "> ", $prebold,
-                  BOLD, $boldtext,
+            my ($beg, $end) = ($-[0], $+[0]);
+            my $prebold  = substr($line, 0, $beg);
+            my $boldtext = substr($line, $beg, length $1);
+            my $postbold = substr($line, $end);
+            print "> ",  $prebold,
+                  BOLD,  $boldtext,
                   RESET, $postbold, "\n";
-        	print "Transform? [Y|all|n]: ";
-	    	chomp(my $answer = lc <STDIN>);
-	    	if ($answer eq '' || $answer eq 'y') {
-		    	$line =~ s/$pattern/$repl/;
-	    	}
-			elsif ($answer eq 'all') {
-		        $line =~ s/$pattern/$repl/;
+            print "Transform? [Y|all|n]: ";
+            chomp(my $answer = lc <STDIN>);
+            if ($answer eq '' || $answer eq 'y') {
+                $line =~ s/$pattern/$repl/;
+            }
+            elsif ($answer eq 'all') {
+                $line =~ s/$pattern/$repl/;
                 $AutoReplace{$match}++;
             }
-		}
+        }
     }
     return $line;    
 }
